be4091a31912395acf694e5f2a0b2274c72e1a86
0yasmin0 <128594377+0yasmin0@users.noreply.github.com>
2024-06-24 11:21:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,42 +6,52 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "você sabe para o que é utilizado a notação cientifica na astronomia?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                
+                texto: "sim, é utilizado para representar as distâncias entre planetas e estrelas",
+                afirmacao: "sim, é utilizado para representar as distâncias entre planetas e estrelas. ",
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                
+                texto: "não faço a mínima ideia para oque serve ",
+                afirmacao: "não faço a mínima ideia para oque serve ",
+            },
+            {
+                
+                texto: "sei boa parte, porem não sei como é corretamente aplicada",
+                afirmacao: "sei boa parte, porem não sei como é corretamente aplicada",
+            },
+          
+          
+        ]
+    },
+    {
+        enunciado: "Qual à emportância da notação cientifica para a ciência?",
+        alternativas: [
+            {
+                texto: "apenas utilizado por preguisa de escrever o número todo.",
+                afirmacao: "apenas utilizado por preguisa de escrever o número todo"
+            },
+            {
+                 texto: "para facilitar a escrita de números extremamente grandes ou extremamente pequenos.",
+                afirmacao: "para facilitar a escrita de números extremamente grandes ou extremamente pequenos.",
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Por que ARQUIMEDES criou a notação cientifica?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "fez uma estimativa para calcular o número de grãos de areia são necessários para preencher o universo",
+                afirmacao: "fez uma estimativa para calcular o número de grãos de areia são necessários para preencher o universo"
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
-            }
-        ]
-    },
-    {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
-        alternativas: [
-            {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "fez uma estimativa para calcular o número de ervilhas em um lote",
+                afirmacao: "fez uma estimativa para calcular o número de ervilhas em um lote"
             },
-            {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
-            }
+            
         ]
     },
    
