d7ffd316928430c05bf967abdfd8cd3b874bbd18
0yasmin0 <128594377+0yasmin0@users.noreply.github.com>
2024-07-31 11:39:53 -0300

    yasmin-projeto31/07

--- script.js
+++ script.js
@@ -6,50 +6,50 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "você sabe para o que é utilizado a notação cientifica na astronomia?",
+        enunciado: "você sabe para o que é utilizado a notação científica na astronomia?",
         alternativas: [
             {
                 
-                texto: "sim, é utilizado para representar as distâncias entre planetas e estrelas",
-                afirmacao: "sim, é utilizado para representar as distâncias entre planetas e estrelas ",
+                texto: "Sim, é utilizado para representar as distâncias entre planetas e estrelas",
+                afirmacao: " É utilizado para representar as distâncias entre planetas e estrelas",
             },
             {
                 
-                texto: "não faço a mínima ideia para oque serve ",
-                afirmacao: "não faço a mínima ideia para oque serve ",
+                texto: "Não faço a mínima ideia para oque serve ",
+                afirmacao: " É utilizado para representar as distâncias entre planetas e estrelas ",
             },
             {
                 
-                texto: "sei boa parte, porem não sei como é corretamente aplicada",
-                afirmacao: "sei boa parte, porem não sei como é corretamente aplicada",
+                texto: "Sei boa parte, porem não sei como é corretamente aplicada",
+                afirmacao: " É utilizado para representar as distâncias entre planetas e estrelas",
             },
           
           
         ]
     },
     {
-        enunciado: "Qual à emportância da notação cientifica para a ciência?",
+        enunciado: "Qual à importância da notação científica para a ciência?",
         alternativas: [
             {
-                texto: "apenas utilizado por preguisa de escrever o número todo.",
-                afirmacao: "apenas utilizado por preguisa de escrever o número todo"
+                texto: "Apenas utilizado por preguisa de escrever o número todo.",
+                afirmacao: "Para facilitar a escrita de números extremamente grandes ou extremamente pequenos."
             },
             {
                  texto: "para facilitar a escrita de números extremamente grandes ou extremamente pequenos.",
-                afirmacao: "para facilitar a escrita de números extremamente grandes ou extremamente pequenos.",
+                afirmacao: "Para facilitar a escrita de números extremamente grandes ou extremamente pequenos.",
             }
         ]
     },
     {
-        enunciado: "Por que ARQUIMEDES criou a notação cientifica?",
+        enunciado: "Por que ARQUIMEDES criou a notação científica?",
         alternativas: [
             {
-                texto: "fez uma estimativa para calcular o número de grãos de areia são necessários para preencher o universo",
-                afirmacao: "fez uma estimativa para calcular o número de grãos de areia são necessários para preencher o universo"
+                texto: "Fez uma estimativa para calcular o número de grãos de areia são necessários para preencher o universo",
+                afirmacao: "Fez uma estimativa para calcular o número de grãos de areia são necessários para preencher o universo"
             },
             {
-                texto: "fez uma estimativa para calcular o número de ervilhas em um lote",
-                afirmacao: "fez uma estimativa para calcular o número de ervilhas em um lote"
+                texto: "Fez uma estimativa para calcular o número de ervilhas em um lote",
+                afirmacao: "Fez uma estimativa para calcular o número de grãos de areia são necessários para preencher o universo"
             },
             
         ]
@@ -60,7 +60,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
-let historiaFinal = "depois dessas perguntas você coneguiu enchergar a importancia da notação cientifica para a ciência e a evolução do entendimento sobre a galaxia ";
+let historiaFinal = "depois dessas perguntas você coneguiu enchergar a importância da notação científica para a ciência e a evolução do entendimento sobre a galáxia ";
 
 function mostraPergunta() {
     if (atual >= perguntas.length) {
